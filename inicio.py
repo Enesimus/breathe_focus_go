010f1699843acdfd60f8fe921bf068a7a42301b7
Juan Sepulveda <jrsepulv@gmail.com>
2026-02-08 15:18:41 -0300

    agrego archivo inicio.py

---
+++ inicio.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title(':target: R_E_S')
+st.subheader('Respira, Enfoca, Suelta')
+st.write(
+    'R_E_S es un proyecto de Rucamanque Arquería que permite registrar el entrenamiento de un arquero de forma cualitativa y cuantitativa.')
+
+
